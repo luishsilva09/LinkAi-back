9c502a6949870bb647370243a83d15da5ff708a3
Luís Henrique <luishsilva09@gmail.com>
2022-09-29 09:14:16 -0300

    feat: add error handler

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,14 @@
 import cors from "cors";
 import express from "express";
+import { errorHandlerMiddleware } from "./middleware/errorHandlerMiddleware";
+import routes from "./routes";
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+
+app.use(routes);
+app.use(errorHandlerMiddleware);
 
 export default app;
