dd95ae7cf5e8de3d8f7c7a89887a4ae1119d2092
Luís Henrique <luishsilva09@gmail.com>
2022-09-29 13:47:08 -0300

    feat:find for a exist user

--- src/repositories/linksRepository.ts
+++ src/repositories/linksRepository.ts
@@ -1,6 +1,10 @@
 import { prisma } from "../dbStrategy/database";
 import { IlinkData } from "../types/linksTypes";
 
+export async function findUser(userId: number) {
+  return await prisma.user.findUnique({ where: { id: userId } });
+}
+
 export async function create(linkData: IlinkData, userId: number) {
   await prisma.link.create({ data: { ...linkData, userId, acessCount: 0 } });
 }
--- src/services/linksService.ts
+++ src/services/linksService.ts
@@ -1,10 +1,18 @@
 import * as linksRepositories from "../repositories/linksRepository";
 import { IlinkData } from "../types/linksTypes";
+import { notFoundError } from "../utils/errorUtils";
+
+async function findUser(userId: number) {
+  const result = await linksRepositories.findUser(userId);
+  if (!result) throw notFoundError("Dados não encontrados");
+}
 
 export async function create(linkData: IlinkData, userId: number) {
+  await findUser(userId);
   await linksRepositories.create(linkData, userId);
 }
 
 export async function get(userId: number) {
+  await findUser(userId);
   return await linksRepositories.get(userId);
 }
